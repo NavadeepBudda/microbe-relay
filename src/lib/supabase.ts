9e50cd509228d005138602dea05c80fc85bc100d
Navadeep Budda <navadeep.budda@gmail.com>
2025-10-28 03:19:33 -0400

    Remove key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jtlhdsumkzmclphdacap.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0bGhkc3Vta3ptY2xwaGRhY2FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2MTk3NDEsImV4cCI6MjA3NzE5NTc0MX0.EqpahJH2m2de4OuAOlg6MwmPQtqKY8DqjseQZJw4XfE'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
